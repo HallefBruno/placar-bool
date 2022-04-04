ec04e4e8db9587f4efd2b67092c614ac92d14221
sud <brunohallef@gmail.com>
2022-04-04 02:09:05 -0300

    Pequenos ajustes

--- src/placarbool/PrincipalUI.java
+++ src/placarbool/PrincipalUI.java
@@ -25,31 +25,31 @@ import org.jsoup.select.Elements;
  */
 public class PrincipalUI extends javax.swing.JFrame {
 
-    Point location;
-    MouseEvent pressed;
-    DefaultComboBoxModel modelo;
-    DefaultListModel modelList;
-    StartPlacar sp;
+  Point location;
+  MouseEvent pressed;
+  DefaultComboBoxModel modelo;
+  DefaultListModel modelList;
+  StartPlacar sp;
 
-    public PrincipalUI() {
-        modelo = new DefaultComboBoxModel();
-        modelList = new DefaultListModel();
+  public PrincipalUI() {
+    modelo = new DefaultComboBoxModel();
+    modelList = new DefaultListModel();
 
-        initComponents();
-        lbaguarde.setVisible(false);
-        setLocationRelativeTo(null);
+    initComponents();
+    lbaguarde.setVisible(false);
+    setLocationRelativeTo(null);
 
-        resultadoList.setCellRenderer(new SelectedListCellRenderer());
+    resultadoList.setCellRenderer(new SelectedListCellRenderer());
 
-        jComboBox1.setModel(modelo);
-        alterarCorCombo();
-        add();
+    jComboBox1.setModel(modelo);
+    alterarCorCombo();
+    add();
 
-        resultadoList.setModel(modelList);
-        
-    }
+    resultadoList.setModel(modelList);
 
-    @SuppressWarnings("unchecked")
+  }
+
+  @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
@@ -215,62 +215,64 @@ public class PrincipalUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnexitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnexitActionPerformed
-        Object[] options = {"Não", "Sim"};
-        int n = JOptionPane.showOptionDialog(null, "Sair?", "Placar", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
-        if (n == 1) {
-            System.exit(0);
-        }
+      Object[] options = {"Não", "Sim"};
+      int n = JOptionPane.showOptionDialog(null, "Sair?", "Placar", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
+      if (n == 1) {
+        System.exit(0);
+      }
     }//GEN-LAST:event_btnexitActionPerformed
 
     private void formMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseDragged
-        Component component = evt.getComponent();
-        location = component.getLocation(location);
-        int x = location.x - pressed.getX() + evt.getX();
-        int y = location.y - pressed.getY() + evt.getY();
-        component.setLocation(x, y);
+      Component component = evt.getComponent();
+      location = component.getLocation(location);
+      int x = location.x - pressed.getX() + evt.getX();
+      int y = location.y - pressed.getY() + evt.getY();
+      component.setLocation(x, y);
     }//GEN-LAST:event_formMouseDragged
 
     private void formMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMousePressed
-        pressed = evt;
+      pressed = evt;
     }//GEN-LAST:event_formMousePressed
 
     private void btnstartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnstartActionPerformed
-        if(jTextField1.getText() != null && !jTextField1.getText().isEmpty()) {
-            long temp = Long.parseLong(modelo.getElementAt(jComboBox1.getSelectedIndex()).toString().substring(0, 1));
-            sp = new StartPlacar(temp, true, jTextField1.getText());
-            sp.start();
-            btnstart.setEnabled(false);
-            btnstop.setEnabled(true);
-            lbaguarde.setVisible(true);
-        } else {
-            JOptionPane.showMessageDialog(null, "Por favor insira o link!");
-            jTextField1.setFocusable(true);
-            lbaguarde.setVisible(false);
-        }
+      if (jTextField1.getText() != null && !jTextField1.getText().isEmpty()) {
+        long temp = Long.parseLong(modelo.getElementAt(jComboBox1.getSelectedIndex()).toString().substring(0, 1));
+        sp = new StartPlacar(temp, true, jTextField1.getText());
+        sp.start();
+        btnstart.setEnabled(false);
+        btnstop.setEnabled(true);
+        lbaguarde.setVisible(true);
+      } else {
+        JOptionPane.showMessageDialog(null, "Por favor insira o link!");
+        jTextField1.setFocusable(true);
+        lbaguarde.setVisible(false);
+      }
     }//GEN-LAST:event_btnstartActionPerformed
 
     private void btnstopActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnstopActionPerformed
-        sp.running = false;
-        btnstart.setEnabled(true);
+      sp.running = false;
+      btnstart.setEnabled(true);
+      lbaguarde.setVisible(false);
+      modelList.clear();
     }//GEN-LAST:event_btnstopActionPerformed
 
-    public static void main(String args[]) {
+  public static void main(String args[]) {
 
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PrincipalUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    try {
+      for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+        if ("Nimbus".equals(info.getName())) {
+          javax.swing.UIManager.setLookAndFeel(info.getClassName());
+          break;
         }
-
-        java.awt.EventQueue.invokeLater(() -> {
-            new PrincipalUI().setVisible(true);
-        });
+      }
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+      java.util.logging.Logger.getLogger(PrincipalUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
     }
+
+    java.awt.EventQueue.invokeLater(() -> {
+      new PrincipalUI().setVisible(true);
+    });
+  }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnexit;
@@ -288,122 +290,121 @@ public class PrincipalUI extends javax.swing.JFrame {
     private javax.swing.JList<String> resultadoList;
     // End of variables declaration//GEN-END:variables
 
-    private void alterarCorCombo() {
-        jComboBox1.setRenderer(new Colorir());
+  private void alterarCorCombo() {
+    jComboBox1.setRenderer(new Colorir());
+  }
+
+  private void add() {
+    String[] tempo = {"30 seg  -- não indicado", "60 seg  -- razoável", "120 seg -- indicado"};
+    for (String s : tempo) {
+      modelo.addElement(s);
+    }
+  }
+
+  private class Colorir extends JLabel implements ListCellRenderer {
+
+    @Override
+    public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+      setText(value.toString());
+
+      if (value.toString().contains("30 seg")) {
+        setBackground(Color.RED);
+        jComboBox1.setBackground(Color.RED);
+      } else if (value.toString().contains("60 seg")) {
+        setBackground(Color.ORANGE);
+        jComboBox1.setBackground(Color.ORANGE);
+      } else {
+        setBackground(Color.GREEN);
+        jComboBox1.setBackground(Color.GREEN);
+      }
+
+      if (isSelected) {
+        //setBackground(Color.WHITE);
+      }
+
+      return this;
+    }
+  }
+
+  public class StartPlacar extends Thread {
+
+    private long temp;
+    private boolean running;
+    private String link;
+
+    public StartPlacar(long temp, boolean running, String link) {
+      this.temp = temp;
+      if (this.temp == 02) {
+        this.temp = 2600;
+      } else if (this.temp == 30) {
+        this.temp = 300;
+      } else {
+        this.temp = 1000;
+      }
+      this.running = running;
+      this.link = link;
     }
 
-    public void add() {
-        String[] tempo = {"30 seg  -- não indicado", "60 seg  -- razoável", "120 seg -- indicado"};
-        for (String s : tempo) {
-            modelo.addElement(s);
-        }
+    public StartPlacar(boolean running) {
+      this.running = running;
     }
 
-    private class Colorir extends JLabel implements ListCellRenderer {
-
-        @Override
-        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-            setText(value.toString());
-
-            if (value.toString().contains("30 seg")) {
-                setBackground(Color.RED);
-                jComboBox1.setBackground(Color.RED);
-            } else if (value.toString().contains("60 seg")) {
-                setBackground(Color.ORANGE);
-                jComboBox1.setBackground(Color.ORANGE);
-            } else {
-                setBackground(Color.GREEN);
-                jComboBox1.setBackground(Color.GREEN);
-            }
-
-            if (isSelected) {
-                //setBackground(Color.WHITE);
-            }
-
-            return this;
+    @Override
+    public void run() {
+      try {
+        while (this.running) {
+          Thread.sleep(this.temp);
+          readPageHtml(this.link);
         }
+      } catch (IOException | InterruptedException ex) {
+        btnstart.setEnabled(true);
+        btnstop.setEnabled(false);
+        this.running = false;
+        JOptionPane.showMessageDialog(null, "Link inválido!");
+        Logger.getLogger(PrincipalUI.class.getName()).log(Level.SEVERE, null, ex);
+      }
     }
 
-    public class StartPlacar extends Thread {
+  }
 
-        private long temp;
-        private boolean running;
-        private String link;
+  public void readPageHtml(String link) throws IOException {
+    //String webPage = "https://www.google.com/search?client=ubuntu&hs=c4k&channel=fs&ei=abvgX_36PKyy5OUPqfyDqAM&q=goi%C3%A1s+x+s%C3%A3o+paulo&oq=goias+x+sao&gs_lcp=CgZwc3ktYWIQARgAMgQIABBHMgQIABBHMgQIABBHMgQIABBHMgQIABBHMgQIABBHMgQIABBHMgQIABBHUABYAGDspgNoAHACeACAAQCIAQCSAQCYAQCqAQdnd3Mtd2l6yAEIwAEB&sclient=psy-ab";
+    //https://www.google.com.br/search?source=hp&ei=MFviX63NDp3Y5OUPnIOGgAk&q=goi%C3%A1s+x+corinthians&oq=GOIASX&gs_lcp=CgZwc3ktYWIQAxgAMgoIABCxAxCDARAKMgQILhAKMgQIABAKMgQIABAKMgQIABAKMgQIABAKMgQIABAKMgQIABAKMgQIABAKMgcIABCxAxAKOggIABCxAxCDAToICC4QsQMQgwE6BQgAELEDOgIIADoLCAAQsQMQxwEQowI6AgguOgsILhCxAxCDARCTAjoFCC4QsQM6CwgAELEDEMcBEK8BOggIABDHARCvAVDuD1jOGWCoKWgAcAB4AIABvwGIAcAGkgEDMC42mAEAoAEBqgEHZ3dzLXdpeg&sclient=psy-ab
+    Document html = Jsoup.connect(link).get();
+    Elements div = html.getElementsByClass("imso-ani imso_mh__tas");
 
-        public StartPlacar(long temp, boolean running, String link) {
-            this.temp = temp;
-            if (this.temp == 02) {
-                this.temp = 2600;
-            } else if (this.temp == 30) {
-                this.temp = 300;
-            } else {
-                this.temp = 1000;
-            }
-            this.running = running;
-            this.link = link;
-        }
-
-        public StartPlacar(boolean running) {
-            this.running = running;
-        }
-
-        @Override
-        public void run() {
-            try {
-                while (this.running) {
-                    Thread.sleep(this.temp);
-                    readPageHtml(this.link);
-                }
-            } catch (IOException | InterruptedException ex) {
-                btnstart.setEnabled(true);
-                btnstop.setEnabled(false);
-                this.running = false;
-                JOptionPane.showMessageDialog(null, "Link inválido!");
-                Logger.getLogger(PrincipalUI.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-
+    if (!div.isEmpty()) {
+      div = html.getElementsByClass("imso-ani imso_mh__tas");
+      Elements gol = html.getElementsByClass("imso_gs__gs-cont imso-medium-font imso_gs__gs-cont-ed");
+      String times = "Times ####" + div.text() + "####";
+      String gols = "Gols >>>>" + gol.text() + "<<<<";
+      modelList.addElement(times);
+      modelList.addElement(gols);
+    } else {
+      Elements table = html.getElementsByClass("ml-bs-u liveresults-sports-immersive__match-grid");
+      Elements rows = table.select("tr");
+      for (int i = 1; i < rows.size(); i++) {
+        Element row = rows.get(i);
+        Elements cell = row.select("td");
+        Elements span = cell.select("span");
+        System.out.println(span.select("img").attr("alt"));
+        modelList.addElement(cell.text());
+      }
     }
 
-    public void readPageHtml(String link) throws IOException {
-        //String webPage = "https://www.google.com/search?client=ubuntu&hs=c4k&channel=fs&ei=abvgX_36PKyy5OUPqfyDqAM&q=goi%C3%A1s+x+s%C3%A3o+paulo&oq=goias+x+sao&gs_lcp=CgZwc3ktYWIQARgAMgQIABBHMgQIABBHMgQIABBHMgQIABBHMgQIABBHMgQIABBHMgQIABBHMgQIABBHUABYAGDspgNoAHACeACAAQCIAQCSAQCYAQCqAQdnd3Mtd2l6yAEIwAEB&sclient=psy-ab";
-        //https://www.google.com.br/search?source=hp&ei=MFviX63NDp3Y5OUPnIOGgAk&q=goi%C3%A1s+x+corinthians&oq=GOIASX&gs_lcp=CgZwc3ktYWIQAxgAMgoIABCxAxCDARAKMgQILhAKMgQIABAKMgQIABAKMgQIABAKMgQIABAKMgQIABAKMgQIABAKMgQIABAKMgcIABCxAxAKOggIABCxAxCDAToICC4QsQMQgwE6BQgAELEDOgIIADoLCAAQsQMQxwEQowI6AgguOgsILhCxAxCDARCTAjoFCC4QsQM6CwgAELEDEMcBEK8BOggIABDHARCvAVDuD1jOGWCoKWgAcAB4AIABvwGIAcAGkgEDMC42mAEAoAEBqgEHZ3dzLXdpeg&sclient=psy-ab
-        Document html = Jsoup.connect(link).get();
-        Elements div = html.getElementsByClass("imso-ani imso_mh__tas");
-        
-        if(div.size() > 0) {
-            div = html.getElementsByClass("imso-ani imso_mh__tas");
-            Elements gol = html.getElementsByClass("imso_gs__gs-cont imso-medium-font imso_gs__gs-cont-ed");
-            String times = "Times ####"+div.text()+"####";
-            String gols = "Gols >>>>"+gol.text()+"<<<<";
-            modelList.addElement(times);
-            modelList.addElement(gols);
-        } else  {
-            Elements table = html.getElementsByClass("ml-bs-u liveresults-sports-immersive__match-grid");
-            Elements rows = table.select("tr");
-            for (int i = 1; i < rows.size(); i++) {
-                Element row = rows.get(i);
-                Elements cell = row.select("td");
-                Elements span = cell.select("span");
-                System.out.println(span.select("img").attr("alt"));
-                modelList.addElement(cell.text());
-            }
-        }
+  }
 
+  public class SelectedListCellRenderer extends DefaultListCellRenderer {
+
+    @Override
+    public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+      Component c = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+      if (isSelected) {
+        c.setBackground(Color.RED);
+        c.setForeground(Color.WHITE);
+      }
+      return c;
     }
-    
-    public class SelectedListCellRenderer extends DefaultListCellRenderer {
-
-        @Override
-        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-            Component c = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-            if (isSelected) {
-                c.setBackground(Color.RED);
-                c.setForeground(Color.WHITE);
-            }
-            return c;
-        }
-    }
-
+  }
 
 }
